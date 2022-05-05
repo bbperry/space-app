62d73dae42a550ab2fb937c7d7b9087682b12266
Ben <benbperry1@gmail.com>
2022-05-05 11:36:54 -0700

    home page needs tweaking

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,27 @@ import React from 'react';
 function Home() {
   return (
     <>
-        <div className="container">
-      <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel to <span class="fs-900 ff-serif text-white">Space</span></h1>
-      <p>
-        Let's face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </p>
-      <a href="#" class="large-button uppercase ff-serif fs-600 text-dark bg-white">Explore</a>
+      <div class="grid-container grid-container--home home" >
+        <div>
+          <h1 class="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">
+            So, you want to travel to
+            <span class="fs-900 ff-serif text-white d-block">Space</span>
+          </h1>
+          <p>
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div>
+          <a
+            href="#"
+            class="large-button uppercase ff-serif text-dark bg-white"
+          >
+            Explore
+          </a>
+        </div>
       </div>
     </>
   );
